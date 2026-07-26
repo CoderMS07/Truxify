a5efb160f25216ce05ae1f200f192f2b40a3ca3f
Akshita <akpandey2307@gmail.com>
2026-07-26 12:23:29 +0530

    fix: use computed proposal state in onlyActiveProposal modifier
    Fixes #4922

--- blockchain/contracts/DAO.sol
+++ blockchain/contracts/DAO.sol
@@ -103,7 +103,7 @@ contract DAO is Ownable, ReentrancyGuard, Pausable {
     }
 
     modifier onlyActiveProposal(uint256 proposalId) {
-        require(proposals[proposalId].state == ProposalState.ACTIVE, "Proposal not active");
+        require(getProposalState(proposalId) == ProposalState.ACTIVE, "Proposal not active");
         _;
     }
 
@@ -196,7 +196,7 @@ contract DAO is Ownable, ReentrancyGuard, Pausable {
         uint256 votingPower = members[msg.sender].votingPower;
         require(votingPower > 0, "No voting power");
         require(!hasVoted[proposalId][msg.sender], "Already voted");
-        require(proposals[proposalId].state == ProposalState.ACTIVE, "Proposal not active");
+        require(getProposalState(proposalId) == ProposalState.ACTIVE, "Proposal not active");
 
         Proposal storage proposal = proposals[proposalId];
 
