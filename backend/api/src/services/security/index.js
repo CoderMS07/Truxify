38f468888f214d1fdd30a31a8d95935db8dab432
Anshul Jain <anshul23102@iiitd.ac.in>
2026-08-02 13:00:50 +0530

    Remove unnecessary index.js from security subdirectory to avoid merge conflicts

--- backend/api/src/services/security/index.js
+++
@@ -1,9 +0,0 @@
-import KeyManagementService from './keyManagementService.js';
-import KeyRotationService from './keyRotationService.js';
-import AnomalyDetectionService from './anomalyDetectionService.js';
-
-export {
-  KeyManagementService,
-  KeyRotationService,
-  AnomalyDetectionService,
-};
